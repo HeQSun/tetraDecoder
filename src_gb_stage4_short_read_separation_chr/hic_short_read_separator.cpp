afb8e05f7fcdddc1abe358d3ccef15610473cf5e
HeqSun <sunhequan@gmail.com>
2024-02-24 14:12:09 +0800

    Update hic_short_read_separator.cpp

--- src_gb_stage4_short_read_separation_chr/hic_short_read_separator.cpp
+++ src_gb_stage4_short_read_separation_chr/hic_short_read_separator.cpp
@@ -22,6 +22,7 @@
 #include  <sys/stat.h>
 #include    <dirent.h>
 #include    <unistd.h>
+#include      <cerrno> // Include the errno header file
 #include "split_string.h"
 #include   "./gzlib/gzstream.h"
 using namespace std;
